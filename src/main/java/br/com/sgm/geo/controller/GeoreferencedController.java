d7f622a6292e6fb755f8da20ae6c7adc438ff7f1
Lucas Pradella <lucas.pradella@hotmail.com>
2021-02-21 00:05:01 -0300

    Add configuração generate-source baseado no swagger

--- src/main/java/br/com/sgm/geo/controller/GeoreferencedController.java
+++ src/main/java/br/com/sgm/geo/controller/GeoreferencedController.java
@@ -1,30 +1,30 @@
 package br.com.sgm.geo.controller;
 
+import br.com.sgm.geo.api.CityDataApi;
 import br.com.sgm.geo.ibge.client.IBGEClient;
 import br.com.sgm.geo.ibge.response.PopulationModel;
+import br.com.sgm.geo.model.InfoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
-public class GeoreferencedController {
+public class GeoreferencedController implements CityDataApi {
 
     @Autowired
     private IBGEClient ibgeClient;
 
-    @RequestMapping(value = "/city-data", method = RequestMethod.GET)
+    @Override
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<?> cityData() throws Exception {
-
+    public ResponseEntity<InfoResponse> cityData(Long idLocation) {
         PopulationModel populationModel = ibgeClient.populationSearch("35");
         String populationModel1 = ibgeClient.municipalMeshesSearch("3538709");
 
 
-        return ResponseEntity.ok(populationModel1);
+        return ResponseEntity.ok(new InfoResponse());
+
     }
 }
